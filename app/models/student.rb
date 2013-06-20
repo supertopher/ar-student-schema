885557fa237dbf9a1c78319d4d716d63f59a7699
supertopher <asimplechris@gmail.com>
2013-06-20 11:28:26 -0700

    Removed name function from student.rb model. LIKE A BOSS.

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,17 +3,17 @@ require 'date'
 
 class Student < ActiveRecord::Base
 
-  validates :email, :format => {  :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, 
-                                  :message => "Please enter a valid email address."}
-  validates :email, :uniqueness => true
-  validates :age, :numericality => {  :greater_than => 5  }
-  validates :phone, :format => {  :with => /^(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/,
-                                  :message => "Please enter a valid 10-digit phone number."}
+  # validates :email, :format => {  :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
+  #                                 :message => "Please enter a valid email address."}
+  # validates :email, :uniqueness => true
+  # validates :age, :numericality => {  :greater_than => 5  }
+  # validates :phone, :format => {  :with => /^(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/,
+  #                                 :message => "Please enter a valid 10-digit phone number."}
 
 
-  def name
-    "#{first_name} #{last_name}"
-  end
+  # def name
+  #   "#{first_name} #{last_name}"
+  # end
 
   def age
     now = Date.today
@@ -23,5 +23,5 @@ class Student < ActiveRecord::Base
       now.year - birthday.year - 1
     end
   end
-    
+
 end
