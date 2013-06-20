01679d8600732ea7ecca2c2d0b5dc0c9fe366995
Apprentice <apprentice@dbc04.local>
2013-06-20 11:40:57 -0700

    removed local migration, not intended for last commit

--- db/migrate/20130619_change_students.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../config'
-
-# this is where you should use an ActiveRecord migration to 
-
-class MergeNames < ActiveRecord::Migration
-  def change
-    execute "ALTER TABLE students ADD COLUMN 'name' VARCHAR(128) UPDATE students SET 'name' = CONCAT('first_name', ' ', 'last_name' );"
-  end
-end
